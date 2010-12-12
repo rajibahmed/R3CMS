51d822d5c42313bb61205b8754221fe6b0fafe2d
Rajib Ahmed <l.rajibahmed@gmail.com>
2010-12-12 03:15:07 +0100

    up to half of chapter 7

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,17 +11,39 @@
 #
 
 class User < ActiveRecord::Base
+
+  attr_accessor :password
   
-  attr_accessible :name , :email
+  attr_accessible :name , :email , :password, :password_confirmation
   
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
   validates :name , :presence   => true ,
-                    :length     => {:maximum=>50,:minimum=>4}
+                    :length     => {:within => 4..50}
   validates :email, :presence   => true,
                     :format     => {:with=>email_regex},
                     :uniqueness => true
-  
+ 
+  validates :password,:presence     => true,
+                      :length       => {:within => 6..40},
+                      :confirmation => true
+
+
   scope :is_admin? , where(:admin=>true)
-  
+
+    
+  before_save :encrypt_password
+
+
+  private # below all the private methods are stored
+
+  def encrypt_password
+    self.encrypted_password = encrypt(password)
+  end
+
+  def encrypt
+    "hello world" #temp solution
+  end
+
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,14 @@
 require 'spec_helper'
 
-describe User do
+describe "User" do
 
   before(:each) do
-    @attr = {:name => 'rajib ahmed', :email=>'rajib@d32.com.bd'}
+    @attr = {
+      :name => 'rajib ahmed', 
+      :email=>'rajib@d32.com.bd',
+      :password =>'123456',
+      :password_confirmation =>'123456'
+    }
   end
 
   it "should have all valid attribute" do
@@ -54,5 +59,49 @@ describe User do
    user_with_duplicate_email.should_not be_valid
   end
 
+
+  describe "password validations " do
+
+    it "should require a password" do
+      user_without_pass = User.new(@attr.merge({:password=>"",:password_confirmation=>""})) 
+      user_without_pass.should_not be_valid
+    end
+
+    it "should have matching confirmation password" do
+      user = User.new(@attr.merge({:password_confirmation=>'121' }))  
+      user.should_not be_valid
+    end
+
+    it "should reject short password" do
+      pass = "a"*3
+      user = User.create(@attr.merge({:password=>pass,:password_confirmation=>pass}))
+      user.should_not be_valid
+    end
+
+  
+    
+    it "should reject password longer than 40 characters " do
+      pass = "a"*41
+      user = User.new(@attr.merge({:password=>pass,:password_confirmation =>pass}))
+      user.should_not be_valid
+    end
+  
+  end
+
+  
+  describe "password encryption " do
+    before(:each) do
+      @user  = User.create!(@attr)
+    end   
+    
+    it "should have encrypte column" do
+      @user.should respond_to :encrypted_password
+    end
+    
+    it "should have encrypted password" do
+      @user.should_not be_blank
+      
+    end
+  end
      
 end
